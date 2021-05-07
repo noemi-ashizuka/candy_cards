be84ed4dd3da15fa07a02620a782878fbbbbdf1b
Noemi Ashizuka <noemi.calanducci@gmail.com>
2021-05-07 10:32:59 +0900

    changed javascript

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const panels = document.querySelectorAll('.panel');
+const activePanel = document.querySelector('.active');
 
 panels.forEach((panel) => {
   panel.addEventListener('click', () => {
-    removeActiveClasses()
-    panel.classList.add('active')
+    activePanel.classList.remove('active');
+    panel.classList.add('active');
   });
 });
-
-const removeActiveClasses = () => {
-  panels.forEach(panel => {
-    panel.classList.remove('active');
-  });
-};
