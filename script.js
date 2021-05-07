a144da45fe20d9b389e57db8b9f8f6f759bfcdbc
Noemi Ashizuka <noemi.calanducci@gmail.com>
2021-05-07 14:40:28 +0900

    removed iteration

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 const panels = document.querySelectorAll('.panel');
-const activePanel = document.querySelector('.active');
 
 panels.forEach((panel) => {
   panel.addEventListener('click', () => {
-    removeActiveClasses()
-    panel.classList.add('active')
+    const activePanel = document.querySelector('.active');
+    activePanel.classList.remove('active');
+    panel.classList.add('active');
   });
 });
-
-const removeActiveClasses = () => {
-  panels.forEach(panel => {
-    panel.classList.remove('active');
-  });
-};
